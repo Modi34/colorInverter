97e486c79e2908a0d4c1e25f63aabc2ce40f3e0c
Oleg Buylov <modi34@yandex.ru>
2021-11-29 20:22:05 +0300

    fixed images and videos for light backgrounds

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ let styleNode = document.createElement('style')
 
 function invertStyles(params){
 	let {isEnabled, hue = 0, invert = 85} = params;
-	styleNode.textContent = `html{filter: invert(${invert}%) hue-rotate(${hue}deg); background: #fff} video,img{filter: invert(1) hue-rotate(${365-hue}deg);}`
+	styleNode.textContent = `html{filter: invert(${invert}%) hue-rotate(${hue}deg); background: #fff} video,img{filter: invert(${100-(100-invert)}%) hue-rotate(${365-hue}deg);}`
 
 	isEnabled ?
 		document.head.appendChild(styleNode) :
