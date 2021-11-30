c0b562f50ff3d9bf78fe35f8211a0071b486671c
Oleg Buylov <modi34@yandex.ru>
2021-11-30 08:41:08 +0300

    Optimised mutation observer

--- content.js
+++ content.js
@@ -1,39 +1,37 @@
 let hostname = document.location.hostname;
+let xpathExpression = new XPathEvaluator().createExpression(`//*[not(contains(@class, 'colorInverter')) and contains(@style, 'url(')]`)
+let isObserving = false;
 
 let styleNode = document.createElement('style')
+let observer = new MutationObserver(scanBackgroundImageNodes);
 
-let isCurrentlyEnabled = false;
 function invertStyles(params){
 	if(!params){return}
 	let {isEnabled, hue = 0, invert = 85} = params;
-	let filter = `invert(${100-(100-invert)}%) hue-rotate(${365-hue}deg)`;
-	styleNode.textContent = `html{filter: invert(${invert}%) hue-rotate(${hue}deg); background: #fff} video,img,svg{filter:${filter};}`
+	styleNode.textContent = `html{filter: invert(${invert}%) hue-rotate(${hue}deg); background: #fff} video,img,svg,.colorInverter{filter: invert(${100-(100-invert)}%) hue-rotate(${365-hue}deg);}`
 
-	isEnabled ?
-		document.head.appendChild(styleNode) :
+	if(isEnabled){
+		!isObserving && observer.observe(document, {childList: true, subtree: true})
+		isObserving = true;
+
+		document.head.appendChild(styleNode)
+		scanBackgroundImageNodes()
+	}else{
+		observer.disconnect();
+		isObserving = false
+
 		styleNode.remove()
+	}
 
-	isCurrentlyEnabled = true;
-	scanBackgroundImageNodes(filter)
 }
 
 chrome.storage.onChanged.addListener(changes => hostname in changes && invertStyles( changes[ hostname ].newValue ));
 chrome.storage.local.get(hostname, data => invertStyles( data[ hostname ] ))
 
-let lastFilter = ''
-let xpathExpression = new XPathEvaluator().createExpression(`//*[contains(@style, 'url(')]`)
-async function scanBackgroundImageNodes(filter, parent = document){
-	lastFilter = filter;
-	let iterator = xpathExpression.evaluate(parent, XPathResult.UNORDERED_NODE_ITERATOR_TYPE);
+async function scanBackgroundImageNodes(){
+	let iterator = xpathExpression.evaluate(document, XPathResult.UNORDERED_NODE_ITERATOR_TYPE);
 	let node;
 	while(node = iterator.iterateNext()){
-		node.style.filter = filter
+		setTimeout(n=>n.classList.add('colorInverter'), 0, node)
 	}
 }
-
-var observer = new MutationObserver(_=>{
-	if(isCurrentlyEnabled && lastFilter){
-		scanBackgroundImageNodes(lastFilter)
-	}
-});
-observer.observe(document, {childList: true, subtree: true});
